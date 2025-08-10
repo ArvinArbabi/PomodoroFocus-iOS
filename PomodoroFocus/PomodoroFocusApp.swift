8b3188e2a100b99ffe8e8a34fb44cbb75d077399
arcinarbavi <arcin.arbavi@gmail.com>
2025-08-09 20:16:38 -0700

    Initial Commit

---
+++ PomodoroFocus/PomodoroFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroFocusApp.swift
+//  PomodoroFocus
+//
+//  Created by Arvin Arbabi on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PomodoroFocusTests/PomodoroFocusTests.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroFocusTests.swift
+//  PomodoroFocusTests
+//
+//  Created by Arvin Arbabi on 8/9/25.
+//
+
+import Testing
+@testable import PomodoroFocus
+
+struct PomodoroFocusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
